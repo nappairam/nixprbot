cace03c59529df48399453aec6dde92d79da959a
Mariappan Ramasamy <1221719+nappairam@users.noreply.github.com>
2026-04-30 18:50:39 +0800

    Add Zig dev flake with hello world app

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Zig development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ zig zls ];
+        };
+      });
+    };
+}
